af79ca43c5018991a8d92eced8be9a6eb5ee3c4c
losfair <zhy20000919@hotmail.com>
2022-04-27 11:47:30 +0000

    fence w, rw

--- fsbl/src/sync.rs
+++ fsbl/src/sync.rs
@@ -2,7 +2,7 @@ use core::arch::asm;
 
 pub unsafe fn io_write<T: Copy>(memory: *mut T, value: T) {
   core::ptr::write_volatile(memory, value);
-  asm!("fence rw, w");
+  asm!("fence w, rw");
 }
 
 pub unsafe fn io_read<T: Copy>(memory: *const T) -> T {
